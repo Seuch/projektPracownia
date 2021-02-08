3ec9f917bb6205cca0439a46a3558fac869ef273
Sebastian Smoluch <swampek0@gmail.com>
2021-02-08 22:45:58 +0100

    initial commit

---
+++ src/main/java/com/example/projektpracownia/ProjektPracowniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektpracownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPracowniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektPracowniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektpracownia/ProjektPracowniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektpracownia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektPracowniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
